d7fb67209432f1ce04004a91c82b541dfd3a17f9
kozaken <gkozaka4@gmail.com>
2025-08-31 21:43:43 +0900

    add:Userモデルの作成とdisplay_nameのバリデーション追加

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :display_name, presence: true, length: { maximum: 20 }
+  validates :display_name, uniqueness: { case_sensitive: false }
+end
