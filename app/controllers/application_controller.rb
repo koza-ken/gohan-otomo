8f53ae1f91cb661e86b8ece4ac6d7119c249ba67
kozaken <gkozaka4@gmail.com>
2025-08-31 22:35:07 +0900

    fix:rubocop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:display_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :display_name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :display_name ])
   end
 end
