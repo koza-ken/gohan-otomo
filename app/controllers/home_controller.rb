6f0009757da3ea1f93ea5f3f93325a729dd04dc5
kozaken <gkozaka4@gmail.com>
2025-09-01 16:18:36 +0900

    add:（仮）Homeコントローラにアニメーションの設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
+    # 初回アクセス時はアニメーションを表示
+    unless session[:welcome_shown]
+      redirect_to welcome_path
+      return
+    end
+    
+    # 通常のトップページ表示
+  end
+
+  def welcome_animation
+    # アニメーション表示フラグを設定
+    session[:welcome_shown] = true
+  end
+
+  def skip_animation
+    # アニメーションをスキップしてトップページへ
+    session[:welcome_shown] = true
+    redirect_to root_path
   end
 end
