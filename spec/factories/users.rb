1a42b2ead4fe2be28d3158b6e56d06a7a5537b63
kozaken <gkozaka4@gmail.com>
2025-09-02 23:43:13 +0900

    add:userモデルのtrait追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -12,5 +12,30 @@ FactoryBot.define do
     trait :without_display_name do
       display_name { nil }
     end
+
+    # プロフィール関連のtrait
+    trait :with_profile do
+      favorite_foods { "白米、納豆、海苔の佃煮、明太子" }
+      disliked_foods { "辛いもの、苦いもの" }
+      profile_public { true }
+    end
+
+    trait :with_detailed_profile do
+      favorite_foods { "白米、納豆、海苔の佃煮、明太子、しらす丼、焼き海苔、梅干し、昆布の佃煮" }
+      disliked_foods { "辛いもの、苦いもの、パクチー、セロリ" }
+      profile_public { true }
+    end
+
+    trait :with_private_profile do
+      favorite_foods { "秘密の食べ物" }
+      disliked_foods { "嫌いなものも秘密" }
+      profile_public { false }
+    end
+
+    trait :without_profile_foods do
+      favorite_foods { "" }
+      disliked_foods { "" }
+      profile_public { true }
+    end
   end
 end
