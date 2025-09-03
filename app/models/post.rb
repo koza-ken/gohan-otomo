f9785dff3ec887d64da4f99f2a8c45bbcabc2e5f
kozaken <gkozaka4@gmail.com>
2025-09-03 22:24:29 +0900

    add:activestorageの紐づけ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
+  
+  # Active Storage: 画像アップロード機能
+  has_one_attached :image
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 200 }
