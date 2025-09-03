d3a942a8c50e86900d9d3483bfcaff7153ea3285
kozaken <gkozaka4@gmail.com>
2025-09-03 10:38:08 +0900

    add:postのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get "profiles/show"
-  get "profiles/edit"
-  get "profiles/update"
   devise_for :users
   root "home#index"
   
   get "welcome", to: "home#welcome_animation"
   post "skip_animation", to: "home#skip_animation"
+  
+  # 投稿機能（認証必須）
+  resources :posts
   
   # プロフィール関連（ネストルーティング）
   resources :users, only: [] do
