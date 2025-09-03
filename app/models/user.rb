ad6c88ccc316fe064df5f1ab0b774b6be744b517
kozaken <gkozaka4@gmail.com>
2025-09-03 09:43:14 +0900

    add:userモデル追加（関連）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts, dependent: :nullify
+  has_many :comments, dependent: :nullify
+
   # バリデーション
   validates :display_name, presence: true, length: { maximum: 20 }
   validates :display_name, uniqueness: { case_sensitive: false }
