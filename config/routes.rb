5c7abee88bec67f6f58d6d6c181ce4634528323c
kozaken <gkozaka4@gmail.com>
2025-09-12 00:23:55 +0900

    add:apiのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,11 @@ Rails.application.routes.draw do
   resources :users, only: [] do
     resource :profile, only: [:show, :edit, :update]
   end
+  
+  # API エンドポイント
+  namespace :api do
+    namespace :rakuten do
+      post 'search_products', to: 'products#search_products'
+    end
+  end
 end
