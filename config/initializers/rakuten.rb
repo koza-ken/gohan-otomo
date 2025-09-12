f01e7ebfba56016c6e31a650c146d2b4759ff125
kozaken <gkozaka4@gmail.com>
2025-09-12 21:41:17 +0900

    fix:テスト環境でも楽天APIをとおすように修正

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -4,7 +4,11 @@
 # 楽天APIクライアントの設定
 # rails起動時にcredentialファイルからAPI IDを取得してくれる
 RakutenWebService.configure do |c|
-  c.application_id = Rails.application.credentials.rakuten[:application_id]
+  # テスト環境や認証情報未設定時の安全な処理
+  rakuten_config = Rails.application.credentials.rakuten || {}
+  
+  c.application_id = rakuten_config[:application_id] || 'dummy_application_id_for_test'
+  
   # アフィリエイトIDがある場合は以下を有効化
-  # c.affiliate_id = Rails.application.credentials.rakuten[:affiliate_id]
+  # c.affiliate_id = rakuten_config[:affiliate_id]
 end
