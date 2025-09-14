e72a78075d24efa55f798f86ef7f7726bb0ac7e3
kozaken <gkozaka4@gmail.com>
2025-09-14 18:13:11 +0900

    fix:＋ボタンの切り替え調整

--- app/javascript/controllers/floating_menu_controller.js
+++ app/javascript/controllers/floating_menu_controller.js
@@ -1,42 +1,46 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "button", "icon"]
-  
+  static targets = ["menu", "button", "plusIcon", "closeIcon"]
+
   connect() {
     // ページ外クリックでメニューを閉じる
     this.boundCloseMenu = this.closeMenu.bind(this)
     document.addEventListener("click", this.boundCloseMenu)
   }
-  
+
   disconnect() {
     document.removeEventListener("click", this.boundCloseMenu)
   }
-  
+
   toggle(event) {
     event.stopPropagation()
-    
+
     if (this.menuTarget.classList.contains("hidden")) {
       this.openMenu()
     } else {
       this.closeMenu()
     }
   }
-  
+
   openMenu() {
     this.menuTarget.classList.remove("hidden")
-    this.iconTarget.style.transform = "rotate(45deg)"
     this.buttonTarget.classList.add("bg-orange-600")
+    // ＋を隠して×を表示
+    this.plusIconTarget.classList.add("hidden")
+    this.closeIconTarget.classList.remove("hidden")
   }
-  
+
   closeMenu(event) {
     // イベントが存在し、かつクリックターゲットがこのコントローラー内の要素の場合は閉じない
     if (event && this.element.contains(event.target)) {
       return
     }
-    
+
     this.menuTarget.classList.add("hidden")
-    this.iconTarget.style.transform = "rotate(0deg)"
     this.buttonTarget.classList.remove("bg-orange-600")
+    // ×を隠して＋を表示
+    this.plusIconTarget.classList.remove("hidden")
+    this.closeIconTarget.classList.add("hidden")
   }
 }
