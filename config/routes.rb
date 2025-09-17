87ac10e39a016becf3cbfcc809f9104b3977e97d
kozaken <gkozaka4@gmail.com>
2025-09-17 17:10:15 +0900

    fix:リファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  
-  
+
   # 投稿機能（認証必須）
   resources :posts do
     resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
   end
-  
+
   # プロフィール関連（ネストルーティング）
   resources :users, only: [] do
     resource :profile, only: [:show, :edit, :update]
   end
-  
+
   # 静的ページ
   get 'terms_of_service', to: 'static_pages#terms_of_service'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
-  
+
   # API エンドポイント
   namespace :api do
     namespace :rakuten do
