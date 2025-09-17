bb8b6e4073c8aaa96d400317a1cc5d2f5eb7f738
kozaken <gkozaka4@gmail.com>
2025-09-17 16:49:00 +0900

    remove:アニメーションのルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   
-  get "welcome", to: "posts#welcome_animation"
-  post "skip_animation", to: "posts#skip_animation"
   
   # 投稿機能（認証必須）
   resources :posts do
